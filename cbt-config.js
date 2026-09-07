8cd72cf99f6e925652a60f3b84c4fa8056044496
ypwd <yayasanwidyadirgantara@gmail.com>
2026-09-07 18:44:29 +0700

    Add files via upload

---
+++ cbt-config.js
@@ -0,0 +1,8 @@
+// CBT Sekolah - GitHub Frontend Configuration
+// Ganti API_URL dan URL halaman jika repo Anda menggunakan path berbeda.
+window.CBT_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbzLgCrzCruUt4Q1bR1b3kdCyQVL1RcNQOHUbz0TSwStoTjYiYUmuex_r3AnI0l07kGk/exec',
+  INDEX_URL: './index.html',
+  GURU_URL: './guru.html',
+  PENGAWAS_URL: './pengawas.html'
+};
